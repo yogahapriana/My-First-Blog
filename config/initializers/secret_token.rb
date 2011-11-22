eadca28efd20be510675bfed3ae5a61f542fa086
Yoga Hapriana <yoga@41studio.com>
2011-11-22 15:07:01 +0700

    My First Blog

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'a7f7cf01533f39cc46a3e021124a830cb25138e8194ac4fd69153207a4576250e4bc81dcf960a2fc311ff2bcbf29341b6f6efd6749aee92674bb384485bbd47b'
